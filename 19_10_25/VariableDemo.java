a43ba66afe7f839566071bbe5dbb950a775204a3
LYHccc <1587168434@qq.com>
2019-10-25 20:51:46 +0800

    变量演示

---
+++ 19_10_25/VariableDemo.java
@@ -0,0 +1,17 @@
+public class VariableDemo{
+	public static void main(String[] args){
+		int a;
+		a = 13;
+		System.out.println(a);
+		
+		byte minByte = Byte.MIN_VALUE;
+		byte maxByte = Byte.MAX_VALUE;
+		System.out.println(minByte);
+		System.out.println(maxByte);
+		
+		int minInt = Integer.MIN_VALUE;
+		int maxInt = Integer.MAX_VALUE;
+		System.out.println(minInt);
+		System.out.println(maxInt);
+	}
+}
