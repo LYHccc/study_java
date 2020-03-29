47bf73ad7abcef3a8b4c94b0bddd93fb5a44193b
LYHccc <1587168434@qq.com>
2020-03-29 21:56:07 +0800

    1.Sine之舞 2.时间转换 3.字符串对比

---
+++ 20_03_29 std/src/TimeTransformation.java
@@ -0,0 +1,28 @@
+/*时间转换
+问题描述:
+　　给定一个以秒为单位的时间t，要求用“<H>:<M>:<S>”的格式来表示这个时间。<H>表示时间，<M>表示分钟，而<S>表示秒，它们都是整数且没有前导的“0”。例如，若t=0，则应输出是“0:0:0”；若t=3661，则输出“1:1:1”。
+输入格式:
+　　输入只有一行，是一个整数t（0<=t<=86399）。
+输出格式:
+　　输出只有一行，是以“<H>:<M>:<S>”的格式所表示的时间，不包括引号。
+ */
+import java.util.Scanner;
+
+public class TimeTransformation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int time = scanner.nextInt();
+        int hour = 0, minute = 0, second = 0;
+        while(time > 0){
+            int temp = time - 3600;
+            if(temp >= 0){
+                hour++;
+            }else{
+                minute = time / 60;
+                second = time - minute * 60;
+            }
+            time -= 3600;
+        }
+        System.out.println(hour + ":" + minute + ":" + second);
+    }
+}
